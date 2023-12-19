1b3e582925a8edd5b6dcf9805ae6ebe120490025
GNOGz <gong6321@gmail.com>
2023-12-19 16:10:19 +0700

    complete10_3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline ;
+    double mew,SD,num,sum = 0.0,sum_2 = 0.0;
+    int n = 0;
+    while(getline(source,textline)){
+
+        num =  atof(textline.c_str()) ;
+        sum += num;
+        sum_2 += num*num;
+        n++;
+    }
+
+    mew = 1.0/n*sum;
+    SD = sqrt((1.0/n*sum_2)-pow(mew,2));
+    cout << "Number of data = "<< n <<endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mew << endl;
+    cout << "Standard deviation = "<< SD << endl;
+}
